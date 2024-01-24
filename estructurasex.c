854d073842810b488ba4c5aa6d3b552df60a51c5
Juan Patino <juan.patino@udla.edu.ec>
2024-01-24 15:48:21 -0500

    Pidiendo al usuario que cantidad de productos se van a comprar

--- estructurasex.c
+++ estructurasex.c
@@ -12,3 +12,13 @@ struct producto
 int ingresarDatos(struct producto *compra);
 int mostrarDatos(struct producto *compra);
 
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Ingrese la cantidad de productos: ");
+    fflush(stdout);
+    scanf("%d", &n);
+
+    struct producto compra[n];
+
+    
